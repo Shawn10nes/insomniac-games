ff5bccf8efce89d7f771a8666e73794b55ded3ef
Shawn <sdj0016@mix.wvu.edu>
2022-01-24 07:15:34 -0500

    Added two footers and fixed topnavbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TopNavBarComponent } from 'src/app/Header/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { BottomFooterComponent } from './Footer/bottom.footer.component';
+import { FooterComponent } from './Footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    FooterComponent,
+    BottomFooterComponent
   ],
   imports: [
     BrowserModule
