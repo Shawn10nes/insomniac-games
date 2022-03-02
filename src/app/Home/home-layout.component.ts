2a8f23d37b9e34aad9026d46f2c9e2f44942691e
Shawn <sdj0016@mix.wvu.edu>
2022-03-01 19:51:24 -0500

    Lab Assignment 7 work

--- src/app/Home/home-layout.component.ts
+++ src/app/Home/home-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { GameItemModel } from "../Spider-Man Card/game-item.model";
+import { GamesService } from "../Spider-Man Card/Games Service/games.service";
 import { mock_game_list } from "../Spider-Man Card/mock_game_list";
 
 @Component({
@@ -10,9 +11,18 @@ import { mock_game_list } from "../Spider-Man Card/mock_game_list";
 export class HomeLayoutComponent{
     games: GameItemModel [] = [];
 
-    constructor(){
-      for(var game of mock_game_list){
-        this.games.push(game);
-      }
+    constructor(private gamesService:GamesService){
+      
+    }
+
+    ngOnInit(): void {
+      this.gamesService.getGames().subscribe(data => {
+        console.log("Fetching game data");
+        for (var game of data) {
+          console.log(game);
+          this.games.push(game);
+        }
+
+      })
     }
 }
---
+++ src/app/Spider-Man Card/Games Service/games.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { GameItemModel } from "../game-item.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GamesService {
+    private baseUrl:string = "https://insomniac-games-default-rtdb.firebaseio.com/"
+    private gamesEndpoint:string = "games.json";
+
+constructor(private http:HttpClient){
+
+}
+
+public getGames() {
+    return this.http.get<GameItemModel []>(this.baseUrl + this.gamesEndpoint);
+}
+
+public getGame(index:number) {
+    return this.http.get<GameItemModel>(this.baseUrl + 'games' + index + '.json')
+}
+
+}
